2f88ae058c84f29fb8c5d1fa4fc6f5e09907d2cb
Matheus Kisiolar <matheuskisiolar@gmail.com>
2023-02-09 16:18:24 -0300

    add functions to paint with mouse click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
 
-let boxesPerSide = 16;
+let boxesPerSide = 32;
 
 const totalBoxes = boxesPerSide * boxesPerSide;
 
@@ -9,9 +9,20 @@ for (let i = 0; i < totalBoxes; i++) {
     box.classList.add("box");
     box.style = `width: ${512/boxesPerSide}px; height: ${512/boxesPerSide}px;`;
     box.addEventListener("mouseover", paint);
+    box.addEventListener("click", paintOne);
     container.appendChild(box);
 }
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 function paint(e){
+    if (mouseDown){
+        e.target.style.backgroundColor = "black";
+    }  
+}
+
+function paintOne(e){
     e.target.style.backgroundColor = "black";
 }
